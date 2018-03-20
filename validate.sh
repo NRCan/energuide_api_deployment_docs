3280b414be226e28f25f75c506d503b9baeb37d7
Steven Zinck <szinck@gmail.com>
2018-03-20 10:31:36 -0300

    move commands to validate.sh

---
+++ validate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+APPS="deploy_etl" 
+RESOURCE_GROUP="NRCanGroup"
+
+
+az login --service-principal -u ${AZURE_SERVICE_PRINCIPAL} --tenant ${AZURE_TENANT_ID} -p ${AZURE_SERVICE_PRINCIPAL_PASSWORD}
+
+for i in $APPS; do
+  az group deployment validate --template-file ${i}.json --resource-group ${RESOURCE_GROUP} --parameters ${i}-params.json 
+done
+
