669f958f38a4a31c81fd0080e0ed80cb6f18fecd
Steven Zinck <szinck@gmail.com>
2018-03-21 08:27:18 -0300

    exit if az login fails

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,7 +6,12 @@ RESOURCE_GROUP="NRCanGroup"
 
 az login --service-principal -u ${AZURE_SERVICE_PRINCIPAL} --tenant ${AZURE_TENANT_ID} -p ${AZURE_SERVICE_PRINCIPAL_PASSWORD}
 
+if [ $? -eq 1 ]; then
+  exit 1
+fi
+
 for i in $APPS; do
+  echo "Validating ${i}......"
   az group deployment validate --template-file ${i}.json --resource-group ${RESOURCE_GROUP} --parameters ${i}-params.json 
 done
 
